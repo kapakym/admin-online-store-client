11d70cc5951b00372e3fe01444c5572c7a6c9360
Alex P <mycop14@mail.ru>
2022-06-16 16:27:21 +0300

    Настроил шаблон клиента

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
 
-createApp(App).use(store).use(router).mount('#app')
+// Подключаем корневой компонент в точку входа в приложение
+createApp(App).mount("#app");
