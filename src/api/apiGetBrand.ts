8291493d21e73cc184613296f1de3105c22c2a71
Alex P <mycop14@mail.ru>
2022-07-18 17:37:17 +0300

    Рефакторинг приложения

--- src/api/apiGetBrand.ts
+++ src/api/apiGetBrand.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { ref } from "vue";
 
-export default async function apiGetBrand() {
+export default async function apiGetBrand():Promise<{}> {
   const responseBrand: any = ref([]);
   try {
     const response = await axios.get("http://localhost:7000/product-brand");
-    console.log(response.data);
+    console.log("data", response.data);
     responseBrand.value = response.data;
-    return responseBrand;
   } catch (error) {
     console.log(error);
   }
+  return responseBrand;
 }
--- src/pages/BrandProduct.vue
+++ src/pages/BrandProduct.vue
@@ -1,13 +1,29 @@
-<template lang="">
-  <div>Производитель</div>
+<template lang="html">
+  <div>
+    <ps-dialog v-model:show="dlgNewBrandVisible">
+      <new-brand @hide="dlgNewBrandVisible=false"/>
+    </ps-dialog>
+  </div>
+  <div>
+    <ps-button @click="dlgNewBrandVisible = true">Добавить</ps-button>
+  </div>
+  Производитель
 </template>
-<script>
-import apiGetBrand from "@/api/apiGetBrand.ts";
+<script lang="ts">
+import apiGetBrand from "@/api/apiGetBrand";
+import NewBrand from "@/components/Brand/NewBrand.vue";
+
 export default {
-  setup(props) {
+  components: {NewBrand},
+  setup(props: any) {
     const responseBrand = apiGetBrand();
-    return responseBrand;
+    return {responseBrand};
+  },
+  data() {
+    return {
+      dlgNewBrandVisible: false,
+    };
   },
 };
 </script>
-<style lang=""></style>
+<style></style>
