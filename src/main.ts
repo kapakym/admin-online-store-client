6bad83f212a6f096e09f31a30cd7239ea12b8458
Alex P <mycop14@mail.ru>
2022-07-31 21:41:24 +0300

    Подключил новый плагин PsPopupPlugin.js и иконки

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "@/App.vue";
 import components from "@/components/UI";
 import router from "@/router/router";
 import store from "@/store";
+import "material-design-icons-iconfont"
+import psPopup from "@/components/plugins/PsPopupPlugin"
+
+
 // console.log(store);
 // import Antd from "ant-design-vue";
 // import "ant-design-vue/dist/antd.css";
 // Подключаем корневой компонент в точку входа в приложение
 const app = createApp(App);
 
+
 components.forEach((component) => {
-  app.component(component.name, component);
+    app.component(component.name, component);
 });
 
 app.use(router);
 app.use(store);
+console.log(app);
+app.use(psPopup);
 app.mount("#app");
 // app.use(Antd).mount("#app");
