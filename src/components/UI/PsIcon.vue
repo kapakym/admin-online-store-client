ec027264353ff5cac42617b2619932b325ffeac3
Alex P <mycop14@mail.ru>
2022-08-02 14:54:11 +0300

    Компонент для отображения иконок

---
+++ src/components/UI/PsIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <a class="material-icons psicon">{{ name }}</a>
+</template>
+
+<script>
+export default {
+  name: "PsIcon",
+  props: {
+    name: String
+  }
+
+}
+</script>
+
+<style scoped>
+
+.psicon {
+  color: #555;
+  /*border: 1px solid red*/
+}
+
+</style>
