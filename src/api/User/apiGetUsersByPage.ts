a7f89c4aa87cdfa16141552194d45415c599194c
Alex P <mycop14@mail.ru>
2022-08-04 12:25:48 +0300

    Рефакторинг кода

--- src/api/User/apiGetUsersByPage.ts
+++ src/api/User/apiGetUsersByPage.ts
@@ -7,7 +7,7 @@ export default async function apiGetUsersByPage(page: number, limit: number) {
     try {
         console.log(limit, page)
         response.value = await axios.get(
-            "http://localhost:7000/users/page", {params: {limit: limit, page: page}}
+            "http://localhost:7000/users/page", {params: {limit: limit, page: page - 1}}
         );
         console.log(response.value)
     } catch (error) {
