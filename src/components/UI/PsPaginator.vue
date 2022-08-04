e7d0556e23bccc18ee45b7fad3413ce7e164f8ca
Alex P <mycop14@mail.ru>
2022-08-04 12:27:45 +0300

    Новый компонент, который выполняет пагинацию между данными

---
+++ src/components/UI/PsPaginator.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pspaginator">
+    <ps-button v-if="currentPage>1">
+      <ps-icon :name="'arrow_back'" @click="returnPage(currentPage-1)"/>
+    </ps-button>
+    <ps-button v-for="n in totalPages" :class="{active: n==currentPage? true :false }" @click="returnPage(n)">{{
+        n
+      }}
+    </ps-button>
+    <ps-button v-if="currentPage<totalPages" @click="returnPage(currentPage+1)">
+      <ps-icon :name="'arrow_forward'"/>
+    </ps-button>
+  </div>
+</template>
+
+<script>
+import PsIcon from "@/components/UI/PsIcon";
+
+export default {
+  name: "PsPaginator",
+  components: {PsIcon},
+  props: {
+    totalPages: Number,
+    currentPage: Number
+  },
+  methods: {
+    returnPage(n) {
+      console.log(n)
+      if (n < 1) n = 1;
+      if (n > this.totalPages) n = this.totalPages
+      this.$emit("changePage", n);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.pspaginator {
+  display: flex;
+  border: 1px solid red;
+  padding: 10px;
+  margin: 10px;
+  justify-content: center;
+}
+</style>
