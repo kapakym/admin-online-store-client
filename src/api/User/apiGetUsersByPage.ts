bd530f25c2f35316285bbefa07807081c23f47af
Alex P <mycop14@mail.ru>
2022-08-04 10:50:34 +0300

    Поменял местами page и limit

--- src/api/User/apiGetUsersByPage.ts
+++ src/api/User/apiGetUsersByPage.ts
@@ -2,12 +2,14 @@ import axios from "axios";
 import {ref} from "vue";
 
 
-export default async function apiGetUsersByPage(limit: number, page: number) {
+export default async function apiGetUsersByPage(page: number, limit: number) {
     const response = ref({});
     try {
+        console.log(limit, page)
         response.value = await axios.get(
             "http://localhost:7000/users/page", {params: {limit: limit, page: page}}
         );
+        console.log(response.value)
     } catch (error) {
         console.log(error);
     }
