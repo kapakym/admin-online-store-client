5c55ffb1ea2ae40b38ad5ce5fe896cba2a7fb2cf
Alex P <mycop14@mail.ru>
2022-08-04 13:10:05 +0300

    Изменил маршрут

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,24 +1,25 @@
 import Main from "@/pages/Main.vue";
-import UserPage from "@/pages/UserPage.vue";
 import UserPageComp from "@/pages/UserPageComposition.vue";
 import Contacts from "@/pages/Contacts.vue";
 import CategoryProducts from "@/pages/CategoryProduct.vue";
 import BrandProduct from "@/pages/BrandProduct.vue";
+import Login from "@/pages/Login"
 
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
-  { path: "", component: Main },
-  { path: "/user", component: UserPage },
-  { path: "/userComp", component: UserPageComp },
-  { path: "/contacts", component: Contacts },
-  { path: "/category", component: CategoryProducts },
-  { path: "/brand", component: BrandProduct },
+    {path: "", component: Main},
+    {path: "/user", component: UserPageComp},
+    {path: "/userComp", component: UserPageComp},
+    {path: "/contacts", component: Contacts},
+    {path: "/category", component: CategoryProducts},
+    {path: "/brand", component: BrandProduct},
+    {path: "/login", component: Login}
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
