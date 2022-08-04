045b77627d9d8d26ca3bc2ec1c936a80407b0b5c
Alex P <mycop14@mail.ru>
2022-08-04 12:28:10 +0300

    Рефакторинг хука. Добавлена возможность работы с пагинатором

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -3,15 +3,18 @@ import apiGetUsersByPage from "@/api/User/apiGetUsersByPage";
 
 export default function useUser(page: number, limit: number) {
     const users = ref([]);
-    const count = ref(Number)
+    const count = ref(Number);
+    const totalPages = ref(Number);
     // Получение всего списка пользователей с сервера
 
     const fetching = async () => {
         try {
-            const response: any = await apiGetUsersByPage(limit, page);
+            const response: any = await apiGetUsersByPage(page, limit);
             console.log(response.value)
             users.value = response.value.data.users;
             count.value = response.value.data.count;
+            // @ts-ignore
+            totalPages.value = Math.ceil(response.value.data.count / limit);
 
         } catch (error) {
             alert("Ошибка");
@@ -21,6 +24,6 @@ export default function useUser(page: number, limit: number) {
     onMounted(fetching);
 
     return {
-        users, count
+        users, count, totalPages
     };
 }
