f13d4c0aa231cd414b983490ee7c2ad3558acd8b
Alex P <mycop14@mail.ru>
2022-08-08 23:51:57 +0300

    Изменил стили. Добавил кнопку для входа и выхода

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,11 +24,15 @@
       </ps-nav-bar-button
       >
 
-      <ps-nav-bar-button @click="logout"
+      <ps-nav-bar-button @click="logout" v-if="$store.state.auth.isAuth"
       >
-        <ps-icon :name="'logout'" style="color: black"/>
+        <ps-icon :name="'logout'" style="color: white"/>
       </ps-nav-bar-button
       >
+      <ps-nav-bar-button @click="$router.push('/login')" v-if="!$store.state.auth.isAuth"
+      >
+        <ps-icon :name="'login'" style="color: white"/>
+      </ps-nav-bar-button>
     </div>
   </div>
 </template>
@@ -36,15 +40,22 @@
 <script>
 import PsNavBarButton from "./UI/PsNavBarButton.vue";
 import PsIcon from "@/components/UI/PsIcon";
+import {mapActions} from "vuex";
 
 export default {
   components: {PsIcon, PsNavBarButton},
+
   setup() {
     return {};
   },
+
   methods: {
+    ...mapActions({
+      checkAuth: "auth/checkAuth"
+    }),
     logout() {
       localStorage.removeItem("token");
+      this.checkAuth();
     }
   }
 };
@@ -52,15 +63,18 @@ export default {
 
 <style lang="css" scoped>
 .navbar {
+  display: flex;
   height: 50px;
   background-color: teal;
   box-shadow: 2px 2px 4px gray;
   display: flex;
   align-items: center;
+
   padding: 0 15px;
 }
 
 .navbar_button {
   margin-left: auto;
+  display: flex;
 }
 </style>
