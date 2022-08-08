16805edd1d144cf7854c8f5ddf6775227fd6b9e7
Alex P <mycop14@mail.ru>
2022-08-08 22:50:04 +0300

    API для авторизации пользователя на сервере

---
+++ src/api/User/apiUserLogin.ts
@@ -0,0 +1,26 @@
+import {ref} from "vue";
+import router from "@/router/router";
+import $api from "@/api/http";
+
+
+interface UserParams {
+    email: string;
+    password: string;
+}
+
+export default async function apiUserLogin(params: UserParams) {
+    const response = ref(<any>{});
+    try {
+        response.value = await $api.post("/auth/login", {email: params.email, password: params.password})
+        console.log(response.value.data.token)
+        // @ts-ignore
+        localStorage.setItem("token", response.value.data.token)
+
+    } catch (error: any) {
+        console.log(error)
+        router.push(`/error/${error.response.data.message}`);
+    }
+
+
+    return response
+}
