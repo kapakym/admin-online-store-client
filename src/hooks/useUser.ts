735c24deb46111b0fa6555a89111980c42c41518
Alex P <mycop14@mail.ru>
2022-08-08 22:52:06 +0300

    Изменены типы переменных

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -3,8 +3,8 @@ import apiGetUsersByPage from "@/api/User/apiGetUsersByPage";
 
 export default function useUser(page: number, limit: number) {
     const users = ref([]);
-    const count = ref(Number);
-    const totalPages = ref(Number);
+    const count = ref(0);
+    const totalPages = ref(0);
     // Получение всего списка пользователей с сервера
 
     const fetching = async () => {
@@ -13,11 +13,10 @@ export default function useUser(page: number, limit: number) {
             console.log(response.value)
             users.value = response.value.data.users;
             count.value = response.value.data.count;
-            // @ts-ignore
             totalPages.value = Math.ceil(response.value.data.count / limit);
 
         } catch (error) {
-            alert("Ошибка");
+            console.log(error);
         }
     };
 
