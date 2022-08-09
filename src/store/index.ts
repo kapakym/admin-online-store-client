e1f3c419155316515004d81fe9f525a515ebf3b5
Alex P <mycop14@mail.ru>
2022-08-10 00:15:29 +0300

    Добавил модуль brandModule

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import {createStore} from "vuex";
 import userModule from "@/store/userModule";
 import authModule from "@/store/authModule";
+import brandModule from "@/store/brandModule";
 
 export default createStore({
     modules: {
         user: userModule,
-        auth: authModule
+        auth: authModule,
+        brand: brandModule
     },
 });
