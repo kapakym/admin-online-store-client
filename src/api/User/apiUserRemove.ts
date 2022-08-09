c49c1d05b3b7a1976562e8f1f0de9a086db801de
Alex P <mycop14@mail.ru>
2022-08-09 22:35:08 +0300

    API для удаления пользователя

---
+++ src/api/User/apiUserRemove.ts
@@ -0,0 +1,26 @@
+import {ref} from "vue";
+import router from "@/router/router";
+import $api from "@/api/http";
+
+interface UserParams {
+    id: number;
+
+}
+
+export default async function apiUserRemove(params: UserParams) {
+    const response = ref({});
+    try {
+
+        response.value = await $api.post(
+            "/users/delete",
+            {
+                userId: params.id,
+            }
+        );
+    } catch (error: any) {
+        router.push(`/error/${error.response.data.message}`);
+    }
+
+
+    return response
+}
