46d2a91276838ac72aecba850de4f31083cea707
Alex P <mycop14@mail.ru>
2022-08-09 22:41:08 +0300

    Добавил action удаления пользователя

--- src/store/userModule.ts
+++ src/store/userModule.ts
@@ -1,31 +1,49 @@
-import axios from "axios";
+import apiGetUsersByPage from "@/api/User/apiGetUsersByPage";
+import apiUserRemove from "@/api/User/apiUserRemove";
 
 const userModule: any = {
-  state: () => ({
-    users: [],
-  }),
-  getters: {},
-  mutations: {
-    setUsers(state: any, users: any) {
-      console.log(state);
-      state.users = users;
-    },
-  },
-  actions: {
-    async fetchUsers({ store, commit }: any) {
-      // Получение всего списка пользователей с сервера
-      try {
-        const response = await axios.get("http://localhost:7000/users");
-        // store.users = response.data;
-        commit("setUsers", response.data);
-        console.log(response);
-      } catch (error) {
-        alert("Ошибка");
-      }
-    },
-  },
+    state: () => ({
+        users: [],
+        page: 1,
+        totalPages: 0,
+        limit: 3
+    }),
+    getters: {},
+    mutations: {
 
-  namespaced: true,
+        setPage(state: any, props: number) {
+            state.page = props;
+        },
+        setTotalPages(state: any, props: number) {
+            state.totalPages = props
+        },
+        setLimit(state: any, props: number) {
+            state.limit = props;
+        },
+        setUsers(state: any, props: any) {
+            state.users = props;
+        }
+    },
+    actions: {
+        async fetchUsers({state, commit}: any, payload: { page: number }) {
+            // Получение всего списка пользователей с сервера
+            if (payload.page) commit("setPage", payload.page)
+            const result: any = await apiGetUsersByPage(state.page, state.limit);
+            if (result.value.data?.users) {
+                commit("setUsers", [...result.value.data.users]);
+                commit("setTotalPages", Math.ceil(result.value.data.count / state.limit))
+            }
+
+        },
+
+        async userRemove({state, dispatch}: any, payload: { id: number }) {
+            const result = await apiUserRemove({id: payload.id});
+            dispatch("fetchUsers", {page: state.page});
+        }
+
+    },
+
+    namespaced: true,
 };
 
 export default userModule;
