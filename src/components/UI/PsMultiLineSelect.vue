8460774c62629881e23b3c2cec35bc631edea456
Alex P <mycop14@mail.ru>
2022-08-09 22:38:20 +0300

    Компонент для множественного выбора

---
+++ src/components/UI/PsMultiLineSelect.vue
@@ -0,0 +1,18 @@
+<template>
+  <select multiple="multiple">
+    <option v-for="item in items"> {{ item }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "PsMultiLineSelect",
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
