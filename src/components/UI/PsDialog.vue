1d65e0ff5b95dc2bb5c4c84509704c2cc0b35602
Alex P <mycop14@mail.ru>
2022-08-10 23:45:17 +0300

    Добавил анимацию появления окна

--- src/components/UI/PsDialog.vue
+++ src/components/UI/PsDialog.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="psdialog" v-if="show" @click="hideDialog">
-    <div @click.stop class="psdialog_content">
-      <slot></slot>
+
+  <transition name="fade-dialog">
+    <div class="psdialog" v-if="show" @click="hideDialog">
+
+      <div @click.stop class="psdialog_content" v-if="show">
+        <slot></slot>
+      </div>
+
     </div>
-  </div>
+  </transition>
+
 </template>
 <script>
 import toggleMixin from "@/mixins/toggleMixin";
@@ -20,9 +26,10 @@ export default {
   bottom: 0;
   right: 0;
   left: 0;
-  background: rgba(0, 0, 0, 0.5);
+  /*background: rgba(0, 0, 0, 0.5);*/
   position: fixed;
   display: flex;
+  text-align: center;
 }
 
 .psdialog_content {
@@ -34,6 +41,28 @@ export default {
   padding: 20px;
   border: 1px solid black;
   box-shadow: 4px 4px 4px black;
-  align-content: space-between;
+}
+
+/*
+enter - появление компонента leave
+enter-active - весит на элементе, то время, за которое его нужно анимировать leave
+enter-to - финальное состояние элемента leave
+ */
+
+/*.fade-dialog-leave-active, .fade-dialog-enter-active {*/
+/*  transition: opacity 1s*/
+/*}*/
+
+/*.fade-dialog-enter-from, .fade-dialog-leave-to {*/
+/*  opacity: 0*/
+/*}*/
+
+.fade-dialog-enter-active, .fade-dialog-leave-active {
+  transition: all .5s;
+}
+
+.fade-dialog-enter-from, .fade-dialog-leave-to {
+  transform: scale(0.5, 0.5);
+  opacity: 0;
 }
 </style>
