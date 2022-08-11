0c31753303aba9a0c7650d2111a76c3421f00253
Alex P <mycop14@mail.ru>
2022-08-11 22:55:49 +0300

    Рефакторинг

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import {createStore} from "vuex";
 import userModule from "@/store/userModule";
 import authModule from "@/store/authModule";
 import brandModule from "@/store/brandModule";
+import categoryModule from "@/store/categoryModule";
 
 export default createStore({
     modules: {
         user: userModule,
         auth: authModule,
-        brand: brandModule
+        brand: brandModule,
+        category: categoryModule
     },
 });
