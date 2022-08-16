167e2476cf481d0acf81e492e9dac4e0847d8a64
Alex P <mycop14@mail.ru>
2022-08-16 17:33:42 +0300

    Страница для работы с шаблонами

---
+++ src/pages/TemplatePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <ps-table :data="test" :headers="head">
+    <template v-slot:td>
+      <ps-button>Hello</ps-button>
+    </template>
+  </ps-table>
+</template>
+
+<script>
+import PsTable from "@/components/UI/PsTable";
+import PsButton from "@/components/UI/PsButton";
+
+export default {
+  name: "TemplatePage",
+  components: {PsButton, PsTable},
+  data() {
+    return {
+      head: ["s", "s", "sss"],
+      test: [{
+        id: 1,
+        name: "dsfasdfadsf"
+      }, {
+        id: 1,
+        name: "dsfasdfadsf"
+      }, {
+        id: 1,
+        name: "dsfasdfadsf"
+      }],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
