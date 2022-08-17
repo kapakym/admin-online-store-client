e3a924ddafe1411fea5a85297bc03496d00e8236
Alex P <mycop14@mail.ru>
2022-08-17 15:06:19 +0300

    Рефакторинг

--- src/components/Templates/TemplateItem.vue
+++ src/components/Templates/TemplateItem.vue
@@ -1,25 +1,27 @@
 <template>
-  
-  <div style="width: 60%">
-    <ps-input v-model="item.name"/>
-  </div>
-  <div>
-    <ps-group-buttons>
-      <ps-button @click="deleteItem">
-        <ps-icon :name="'delete'"/>
-        Удалить
-      </ps-button>
-      <ps-button @click="changeLogo">
-        <ps-icon :name="'add_photo_alternate'"/>
-        Изменить логотип
-      </ps-button>
-      <ps-button @click="update">
-        <ps-icon :name="'save'"/>
-        Сохранить
-      </ps-button>
-    </ps-group-buttons>
+  <div class="itemtemplate">
 
+    <ps-input v-model="item.name"/>
+
+    <div>
+      <ps-group-buttons>
+        <ps-button @click="deleteItem">
+          <ps-icon :name="'delete'" style="color: red"/>
+          Удалить
+        </ps-button>
+        <ps-button @click="changeLogo">
+          <ps-icon :name="'edit'"/>
+          Редактировать шаблон
+        </ps-button>
+        <ps-button @click="update">
+          <ps-icon :name="'save'" style="color:green"/>
+          Сохранить
+        </ps-button>
+      </ps-group-buttons>
+
+    </div>
   </div>
+
 
 </template>
 
@@ -35,13 +37,16 @@ export default {
 <style scoped>
 .itemtemplate {
   display: flex;
-  /*width: 95%;*/
   border: 1px solid black;
   padding: 5px;
   margin: 5px;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   border-radius: 5px;
+}
+
+.itemtemplate:hover {
+  background: lightgoldenrodyellow;
 }
 
 
