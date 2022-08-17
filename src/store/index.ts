ef4b56fc83a80dae8287d28d33a993e6f0a8d02e
Alex P <mycop14@mail.ru>
2022-08-17 13:17:31 +0300

    Подключил модуль templateModule.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,14 @@ import userModule from "@/store/userModule";
 import authModule from "@/store/authModule";
 import brandModule from "@/store/brandModule";
 import categoryModule from "@/store/categoryModule";
+import templateModule from "@/store/templateModule";
 
 export default createStore({
     modules: {
         user: userModule,
         auth: authModule,
         brand: brandModule,
-        category: categoryModule
+        category: categoryModule,
+        template: templateModule
     },
 });
