0224414347ae7d49ab1f06c08cdcee81bffcd7e5
Alex P <mycop14@mail.ru>
2022-08-17 15:05:26 +0300

    коспонент для работы со свойствами шаблонов

---
+++ src/components/Templates/EditPropertyForm.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+  name: "EditPropertyForm",
+  computed: {
+    ...mapState({})
+  },
+  data() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
