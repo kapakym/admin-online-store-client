4bedd4d04a54ca954d6d0c594aedb9f6441dca2c
Alex P <mycop14@mail.ru>
2022-08-17 13:17:11 +0300

    Добавил новые компоненты и пордключил переменные из стора

--- src/pages/TemplatePage.vue
+++ src/pages/TemplatePage.vue
@@ -1,20 +1,39 @@
 <template>
-  <ps-table :data="test" :headers="head">
-    <template v-slot:td>
-      <ps-button>Hello</ps-button>
-    </template>
-  </ps-table>
+  <ps-dialog v-model:show="visibleDialogNewCaregory">
+    <new-template-from @close="visibleDialogNewCaregory=false"/>
+  </ps-dialog>
+  <ps-button @click="visibleDialogNewCaregory=true">
+    <ps-icon :name="'add'"/>
+    Добавить
+  </ps-button>
+  <template-item v-for="item in templates" :item="item" v-if="templates.length>0"/>
+  <div><h1>Нет ни одного шаблона!</h1></div>
+
 </template>
 
 <script>
 import PsTable from "@/components/UI/PsTable";
 import PsButton from "@/components/UI/PsButton";
+import PsIcon from "@/components/UI/PsIcon";
+import PsDialog from "@/components/UI/PsDialog";
+import NewTemplateFrom from "@/components/Templates/NewTemplateFrom";
+import {mapState} from "vuex";
+import TemplateItem from "@/components/Templates/TemplateItem";
 
 export default {
   name: "TemplatePage",
-  components: {PsButton, PsTable},
+  components: {TemplateItem, NewTemplateFrom, PsDialog, PsIcon, PsButton, PsTable},
+  computed: {
+    ...mapState({
+      page: state => state.template.page,
+      limit: state => state.template.limit,
+      templates: state => state.template.templates,
+      totalPages: state => state.template.totalPages
+    })
+  },
   data() {
     return {
+      visibleDialogNewCaregory: false,
       head: ["s", "s", "sss"],
       test: [{
         id: 1,
