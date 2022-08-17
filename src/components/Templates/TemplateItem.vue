d4b953298e55fa5f527d03ef386a75f28aff40da
Alex P <mycop14@mail.ru>
2022-08-17 13:16:41 +0300

    Компонент для отображения имени шаблона

---
+++ src/components/Templates/TemplateItem.vue
@@ -0,0 +1,48 @@
+<template>
+  
+  <div style="width: 60%">
+    <ps-input v-model="item.name"/>
+  </div>
+  <div>
+    <ps-group-buttons>
+      <ps-button @click="deleteItem">
+        <ps-icon :name="'delete'"/>
+        Удалить
+      </ps-button>
+      <ps-button @click="changeLogo">
+        <ps-icon :name="'add_photo_alternate'"/>
+        Изменить логотип
+      </ps-button>
+      <ps-button @click="update">
+        <ps-icon :name="'save'"/>
+        Сохранить
+      </ps-button>
+    </ps-group-buttons>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "TemplateItem",
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+.itemtemplate {
+  display: flex;
+  /*width: 95%;*/
+  border: 1px solid black;
+  padding: 5px;
+  margin: 5px;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 5px;
+}
+
+
+</style>
