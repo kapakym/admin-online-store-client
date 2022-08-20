f2d08d6d71291270235b7f32005e3deb616dd5ab
Alex P <mycop14@mail.ru>
2022-08-20 23:58:46 +0300

    Компонент для отображения чекбокса и пояснения к нему

---
+++ src/components/UI/PsCheckBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <input type="checkbox"/>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "PsCheckBox",
+  props: {
+    modelValue: Boolean,
+    label: String
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
