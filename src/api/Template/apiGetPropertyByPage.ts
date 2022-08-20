e315540f526041e46262539c220b29ebc6d776c4
Alex P <mycop14@mail.ru>
2022-08-20 23:57:04 +0300

    api для работы со свойствами шаблонов

--- src/api/Template/apiGetPropertyByPage.ts
+++ src/api/Template/apiGetPropertyByPage.ts
@@ -1,14 +1,19 @@
-import {ref} from "vue";
+import { ref } from "vue";
 import router from "@/router/router";
 import $api from "@/api/http";
 
-export default async function apiGetTemplateByPage(page: number, limit: number): Promise<{}> {
-    const response: any = ref({});
-    try {
-        response.value = await $api.get("/template/property/page", {params: {limit: limit, page: page - 1}});
-
-    } catch (error: any) {
-        await router.push(`/error/${error.response.data.message}`);
-    }
-    return response;
+export default async function apiGetProperetyByPage(
+  page: number,
+  limit: number,
+  templateId: number
+): Promise<{}> {
+  const response: any = ref({});
+  try {
+    response.value = await $api.get("/template/property/page", {
+      params: { limit: limit, page: page - 1, id: templateId },
+    });
+  } catch (error: any) {
+    await router.push(`/error/${error.response.data.message}`);
+  }
+  return response;
 }
