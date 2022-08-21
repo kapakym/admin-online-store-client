8718b852d2250bed9d5f246ccd28600bb0cef074
Alex P <mycop14@mail.ru>
2022-08-21 22:52:17 +0300

    api для работы со свойствами шаблонов

---
+++ src/api/Template/apiCreateProperty.ts
@@ -0,0 +1,15 @@
+import $api from "@/api/http";
+import router from "@/router/router";
+import {ref} from "vue"
+
+export default async function apiCreateProperty(prop: { templateId: number, data: Array<{ id: number; name: string; type: string; exist: string }> }
+) {
+    const result = ref({});
+    console.log(prop)
+    try {
+        result.value = $api.put("/template/property", prop);
+    } catch (error: any) {
+        await router.push(`/error/${error.response.data.message}`);
+    }
+    return result;
+}
