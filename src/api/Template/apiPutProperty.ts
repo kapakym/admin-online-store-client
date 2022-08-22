13d9c0e053a7469e3333fa6b25ab85e5c3226df3
Alex P <mycop14@mail.ru>
2022-08-22 16:05:15 +0300

    api для отправки запроса на сервер для удаления свойства

--- src/api/Template/apiPutProperty.ts
+++ src/api/Template/apiPutProperty.ts
@@ -2,7 +2,7 @@ import $api from "@/api/http";
 import router from "@/router/router";
 import {ref} from "vue"
 
-export default async function apiCreateProperty(prop: { templateId: number, data: Array<{ id: number; name: string; type: string; exist: string }> }
+export default async function apiPutProperty(prop: { templateId: number, data: Array<{ id: number; name: string; type: string; exist: string }> }
 ) {
     const result = ref({});
     console.log(prop)
