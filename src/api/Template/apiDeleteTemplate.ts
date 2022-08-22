ce479394dadfbfeefc26102451ebd4e01bb80611
Alex P <mycop14@mail.ru>
2022-08-22 22:07:14 +0300

    api для отправки запроса на удаление шаблона

---
+++ src/api/Template/apiDeleteTemplate.ts
@@ -0,0 +1,13 @@
+import $api from "@/api/http";
+import router from "@/router/router";
+import {ref} from "vue";
+
+export default async function apiDeleteTemplate(id: number) {
+    const result = ref({})
+    try {
+        result.value = await $api.delete("/template", {params: {id: id}});
+    } catch (error: any) {
+        await router.push(`/error/${error.response.data.message}`)
+    }
+    return result;
+}
