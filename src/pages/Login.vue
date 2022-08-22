9ea0c957faa8442bfde31e1da8c0db20d817f1d2
Alex P <mycop14@mail.ru>
2022-08-23 00:30:58 +0300

    Работа со стилями

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,32 +2,36 @@
   <div class="container">
     <div class="loginpage">
       <h2>{{ registrationFlag ? "Регистрация" : "Авторизация" }}</h2>
-      <ps-input v-model="email" type="text" placeholder="email" />
-      <ps-input v-model="password" type="text" placeholder="password" />
-      
+      <ps-input v-model="email" type="text" placeholder="email"/>
+      <ps-input v-model="password" type="text" placeholder="password"/>
+
       <ps-input
-        v-if="registrationFlag"
-        v-model="repassword"
-        type="text"
-        @input="validPassword"
-        placeholder="repeat password"
+          v-if="registrationFlag"
+          v-model="repassword"
+          type="text"
+          @input="validPassword"
+          placeholder="repeat password"
       />
       <h3 v-if="passwordNot && registrationFlag" style="color: red">
         Пароли не совпадают
       </h3>
-      <ps-group-buttons>
-        <ps-button
-          @click="registrationFlag ? registrationMethod() : loginMethods()"
-        >
-          <ps-icon :name="'done'" style="color: green" />
-          {{ registrationFlag ? "Создать" : "Вход" }}
-        </ps-button>
-        <ps-button @click="registrationFlag = !registrationFlag"
-          >{{ registrationFlag ? "Авторизация" : "Регистрация" }}
-        </ps-button>
-      </ps-group-buttons>
+      <div class="footer">
+        <a href="">Восстановление пароля</a>
+        <ps-group-buttons>
 
-      <a href="">Восстановление пароля</a>
+          <ps-button
+              @click="registrationFlag ? registrationMethod() : loginMethods()"
+          >
+            <ps-icon :name="'done'" style="color: green"/>
+            {{ registrationFlag ? "Создать" : "Вход" }}
+          </ps-button>
+          <ps-button @click="registrationFlag = !registrationFlag"
+          >{{ registrationFlag ? "Авторизация" : "Регистрация" }}
+          </ps-button>
+        </ps-group-buttons>
+      </div>
+
+
     </div>
   </div>
 </template>
@@ -37,12 +41,12 @@ import PsInput from "@/components/UI/PsInput";
 import PsGroupButtons from "@/components/UI/PsGroupButtons";
 import PsButton from "@/components/UI/PsButton";
 import apiUserLogin from "@/api/User/apiUserLogin";
-import { mapActions } from "vuex";
+import {mapActions} from "vuex";
 import PsIcon from "@/components/UI/PsIcon";
 
 export default {
   name: "LoginPage",
-  components: { PsIcon, PsButton, PsGroupButtons, PsInput },
+  components: {PsIcon, PsButton, PsGroupButtons, PsInput},
   data() {
     return {
       email: "",
@@ -64,7 +68,7 @@ export default {
     },
     async registrationMethod() {
       if (!this.passwordNot)
-        this.registrationUser({ email: this.email, password: this.password });
+        this.registrationUser({email: this.email, password: this.password});
     },
     async loginMethods() {
       const token = await apiUserLogin({
@@ -85,13 +89,14 @@ export default {
   display: flex;
   box-sizing: border-box;
   flex-direction: column;
-  width: 30%;
-  border: 4px solid teal;
+  width: 40%;
+  /*border: 4px solid teal;*/
   /*justify-content: center;*/
   align-items: center;
   /*align-content: center;*/
   border-radius: 10px;
-  box-shadow: 5px 5px 5px black;
+  padding: 10px;
+  box-shadow: 5px 5px 25px black;
 }
 
 .container {
@@ -99,5 +104,13 @@ export default {
   display: flex;
   justify-content: center;
   margin-top: 10%;
+}
+
+.footer {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  white-space: nowrap;
 }
 </style>
