56daee8b795b687a290fa3b70e7bc6f599a31626
Alex P <mycop14@mail.ru>
2022-08-23 11:49:54 +0300

    Создал страницу для работы с товарами

---
+++ src/pages/ProductPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <ps-button>
+    <ps-icon name="add"/>
+    Добавить
+  </ps-button>
+</template>
+
+<script>
+import PsButton from "@/components/UI/PsButton";
+import PsIcon from "@/components/UI/PsIcon";
+
+export default {
+  name: "ProductPage",
+  components: {PsIcon, PsButton}
+}
+</script>
+
+<style scoped>
+
+</style>
