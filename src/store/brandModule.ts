66c00a41c0950c99acf7394ed146b86388bfe36d
Alex P <mycop14@mail.ru>
2022-08-27 11:22:42 +0300

    Исправил работу модуля

--- src/store/brandModule.ts
+++ src/store/brandModule.ts
@@ -48,19 +48,19 @@ const brandModule = {
         },
         async addBrand({state, dispatch}: any, payload: { name: string, file: Blob }) {
             await apiAddBrand({name: payload.name, file: payload.file});
-            dispatch("fetchBrands", {page: state.page})
+            dispatch("fetchBrandsByPage", {page: state.page})
         },
         async removeBrand({state, dispatch}: any, payload: { id: number }) {
             await apiDeleteBrand(payload.id);
-            dispatch("fetchBrands", {page: state.page});
+            dispatch("fetchBrandsByPage", {page: state.page});
         },
         async changePictureBrand({state, dispatch}: any, payload: { id: number, file: Blob }) {
             await apiChangePictureBrand({id: payload.id, file: payload.file});
-            dispatch("fetchBrands", {page: state.page});
+            dispatch("fetchBrandsByPage", {page: state.page});
         },
         async updateBrand({state, dispatch}: any, payload: { id: number, name: string }) {
             await apiUpdateBrand({id: payload.id, name: payload.name})
-            dispatch("fetchBrands", {page: state.page});
+            dispatch("fetchBrandsByPage", {page: state.page});
         }
     },
     namespaced: true
