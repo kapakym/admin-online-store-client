d44011c12350e13e5d32f457df8a8fef09aa3af7
Alex P <mycop14@mail.ru>
2022-08-30 22:02:22 +0300

    Добавил в модуль переменные и методы для работы с характеристиками товара

--- src/store/productModule.ts
+++ src/store/productModule.ts
@@ -1,13 +1,18 @@
 import apiAddProduct from "@/api/Product/apiAddProduct";
 import {ParamsProduct} from "@/api/Product/TypesProduct";
 import apiGetProductByPage from "@/api/Product/apiGetProductByPage";
+import apiGetProductInfoByPage from "@/api/Product/apiGetInfoProduct";
 
 const productModule = {
     state: () => ({
         products: [],
         page: 1,
         limit: 3,
-        totalPages: 0
+        totalPages: 0,
+        info: [],
+        infoPage: 1,
+        infoLimit: 3,
+        infoTotalPage: 0
 
     }),
     getters: {},
@@ -23,6 +28,18 @@ const productModule = {
         },
         setTotalPages(state: any, props: number) {
             state.totalPages = props;
+        },
+        setInfoProductions(state: any, props: []) {
+            state.info = props;
+        },
+        setInfoPage(state: any, props: number) {
+            state.infoPage = props;
+        },
+        setInfoLimit(state: any, props: number) {
+            state.infoLimit = props;
+        },
+        setInfoTotalPages(state: any, props: number) {
+            state.infoTotalPages = props;
         },
     },
     actions: {
@@ -45,7 +62,27 @@ const productModule = {
                 );
                 console.log(result.value.data.count)
             }
+        },
+        async fetchProductInfoByPage({
+                                         state,
+                                         commit
+                                     }: any, payload: { page?: number, limit?: number, productId: number }) {
+            console.log(payload)
+            if (payload?.page) commit("setInfoPage", payload.page);
+            if (payload?.limit) commit("setInfoLimit", payload.limit);
+            const result: any = await apiGetProductInfoByPage(payload.productId, state.page, state.limit);
+
+            if (result.value.data?.productInfo) {
+                console.log(result.value.data);
+                commit("setInfoProductions", [...result.value.data.productInfo]);
+                commit(
+                    "setInfoTotalPages",
+                    Math.ceil(result.value.data.count / state.limit)
+                );
+                console.log(result.value.data.count)
+            }
         }
+
     },
     namespaced: true
 }
