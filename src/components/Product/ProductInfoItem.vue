c92ffb0fc1c20d0b6432efa777a11b2ab6fb5498
Alex P <mycop14@mail.ru>
2022-08-30 21:55:29 +0300

    Компонент для вывода характеристики

---
+++ src/components/Product/ProductInfoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="itemInfoProduct">
+    <ps-label-input v-model="item.value" placeholder="Введите значение">{{ item.name }}</ps-label-input>
+    <div class="typeInfo">{{ item.type }}</div>
+    <ps-group-buttons v-if="!item.templateId">
+      <ps-button>
+        <ps-icon name="delete" style="color: red"/>
+      </ps-button>
+    </ps-group-buttons>
+  </div>
+
+</template>
+
+<script>
+import PsLabelInput from "@/components/UI/PsLabelInput";
+import PsGroupButtons from "@/components/UI/PsGroupButtons";
+import PsButton from "@/components/UI/PsButton";
+
+export default {
+  name: "ProductInfoItem",
+  components: {PsButton, PsGroupButtons, PsLabelInput},
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+.itemInfoProduct {
+  display: flex;
+  align-items: center;
+}
+
+.typeInfo {
+  border: 1px solid black;
+  width: 100px;
+  margin: 5px;
+  padding: 5px;
+  border-radius: 5px;
+}
+</style>
