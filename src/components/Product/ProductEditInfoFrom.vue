1181adcc32f8a737287ed6755654c12287d8dd24
Alex P <mycop14@mail.ru>
2022-08-31 13:32:52 +0300

    Добавил select для изменения примененного шаблона к продукту

--- src/components/Product/ProductEditInfoFrom.vue
+++ src/components/Product/ProductEditInfoFrom.vue
@@ -1,16 +1,21 @@
 <template>
-  <h1>Редактирование характеристик товара</h1>
-  <ps-button>
-    <ps-icon name="add"/>
-    Добавить
-  </ps-button>
-  <ps-paginator/>
+  <h1>Редактирование характеристик товара "{{ product.name }}"</h1>
+  <div class="changeTemplate">
+    <ps-label-select v-model="product.templateId" :model="templates">Используемый шаблон:</ps-label-select>
+    <ps-button @click="changeTemplate">
+      <ps-icon name="save" style="color: blue"/>
+      Изменить
+    </ps-button>
+  </div>
+
+
+  <ps-paginator :total-pages="totalPage" :current-page="page" @changePage="changePage"/>
   <div v-for="item of info">
     <product-info-item :item="item"/>
   </div>
-  <ps-paginator/>
+  <ps-paginator :total-pages="totalPage" :current-page="page" @changePage="changePage"/>
   <ps-group-buttons>
-    <ps-button>
+    <ps-button @click="saveChanges">
       <ps-icon name="save" style="color: blue"/>
       Сохранить
     </ps-button>
@@ -28,12 +33,15 @@ import PsButton from "@/components/UI/PsButton";
 import PsIcon from "@/components/UI/PsIcon";
 import PsPaginator from "@/components/UI/PsPaginator";
 import PsGroupButtons from "@/components/UI/PsGroupButtons";
+import apiGetTemplate from "@/api/Template/apiGetTemplate";
+import apiPutTemplateProduct from "@/api/Product/apiPutTemplateProduct";
+import apiPutInfoProduct from "@/api/Product/apiPutInfoProduct";
 
 export default {
   name: "ProductEditInfoFrom",
   components: {PsGroupButtons, PsPaginator, PsIcon, PsButton, ProductInfoItem},
   props: {
-    productId: Number,
+    product: Object
   },
   emits: ["edit", "close"],
   computed: {
@@ -44,18 +52,43 @@ export default {
       totalPage: state => state.product.infoTotalPage
     })
   },
+  data() {
+    return {
+      templates: []
+    }
+  },
   methods: {
     ...mapActions({
-      getInfo: "product/fetchProductInfoByPage"
-    })
+      getInfo: "product/fetchProductInfoByPage",
+      getTemplates: "template/fetchTemplates"
+    }),
+    async changeTemplate() {
+      this.$popup("ВНИМАНИЕ! При изменении шаблона все характеристики продукта будут удалены", async () => {
+        await apiPutTemplateProduct({templateId: this.product.templateId, productId: this.product.id})
+        await this.changePage(1);
+      });
+     
+    },
+    async changePage(numberPage) {
+      await this.getInfo({productId: this.product.id, page: numberPage});
+    },
+    async saveChanges() {
+      await apiPutInfoProduct({productId: this.product.id, info: this.info});
+    }
   }
-  , mounted() {
-    this.getInfo({productId: this.productId});
-    console.log(this.info)
+  , async mounted() {
+    await this.changePage(1)
+    // await this.getInfo({productId: this.product.id});
+    const templ = await apiGetTemplate();
+    this.templates = templ;
+    console.log(this.product)
   }
 }
 </script>
 
 <style scoped>
+.changeTemplate {
+  display: flex
+}
 
 </style>
